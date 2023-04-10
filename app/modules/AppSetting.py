2f49f8d8f8aa75334f640e7bbe3c38de2a8b2f81
Mohammad Hossein Heydarchi <mohammad.heydarchi@volvocars.com>
2023-04-10 23:43:02 +0200

    Update AppSetting to use environment variable for config folder

--- app/modules/AppSetting.py
+++ app/modules/AppSetting.py
@@ -1,15 +1,16 @@
 import os
 from PythonUtilityClasses.FileWriter import FileWriter
 from PythonUtilityClasses.FileReader import FileReader
+import os
 
 OUT_DIR = "./out/"
-CONFIG_DIR = "./config/"
 API_KET_FILE = "api_key.txt"
 
 
 class AppSetting:
     def __init__(self):
         self.api_key = None
+        self.config_dir = os.environ["CLI_AI_DIR"] + "/config/"
 
     def init(self):
         self._initDirs()
@@ -18,18 +19,18 @@ class AppSetting:
     """If the API key file is not found, get it through the console"""
 
     def _init_api_key(self):
-        if not os.path.exists(CONFIG_DIR + API_KET_FILE):
+        if not os.path.exists(self.config_dir + API_KET_FILE):
             self.api_key = input("Please enter your OpenAI API key: ")
-            FileWriter.write_file(CONFIG_DIR + API_KET_FILE, self.api_key)
+            FileWriter.write_file(self.config_dir + API_KET_FILE, self.api_key)
         else:
-            self.api_key = FileReader.read_file(CONFIG_DIR + API_KET_FILE)
+            self.api_key = FileReader.read_file(self.config_dir + API_KET_FILE)
 
     def _initDirs(self):
         if not os.path.exists(OUT_DIR):
             os.makedirs(OUT_DIR)
 
-        if not os.path.exists(CONFIG_DIR):
-            os.makedirs(CONFIG_DIR)
+        if not os.path.exists(self.config_dir):
+            os.makedirs(self.config_dir)
 
     def get_api_key(self):
         if self.api_key == None:
