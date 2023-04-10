0d335e30ba2651f0946f18915b2306200bccb1fb
Mohammad Hossein Heydarchi <mohammad.heydarchi@volvocars.com>
2023-04-10 16:23:03 +0200

    - Add AppSetting class - Add some test units

---
+++ scripts/AppSetting.py
@@ -0,0 +1,33 @@
+import os
+from scripts.PythonUtilityClasses.FileWriter import FileWriter
+
+OUT_DIR = "out/"
+CONFIG_DIR = "config/"
+API_KET_FILE = "api_key.txt"
+
+class AppSetting:
+    def __init__(self):
+        self.api_key = None
+
+    def init(self):
+        self._initDirs()
+        self._init_api_key()
+
+    '''If the API key file is not found, get it through the console'''
+    def _init_api_key(self):
+        if not os.path.exists(CONFIG_DIR + API_KET_FILE):
+            self.api_key = input("Please enter your OpenAI API key: ")
+            FileWriter.write_file(CONFIG_DIR + API_KET_FILE, self.api_key)
+
+    def _initDirs(self):
+        if not os.path.exists(OUT_DIR):
+            os.makedirs(OUT_DIR)
+
+        if not os.path.exists(CONFIG_DIR):
+            os.makedirs(CONFIG_DIR)
+
+    def get_api_key(self):
+        if self.api_key == None:
+            if self.api_key == None:
+                raise ValueError("API key is not set")
+        return self.api_key
---
+++ test/AppSetting_test.py
@@ -0,0 +1,15 @@
+#This is a test file for the AppSetting class
+import unittest
+import pytest
+from scripts.AppSetting import AppSetting
+
+class MyTestCase(unittest.TestCase):
+
+    def test_init(self):
+        app_setting = AppSetting()
+        self.assertEqual(app_setting.api_key, None)
+
+    def test_get_api_key_raises_exception(self):
+        app_setting = AppSetting()
+        with pytest.raises(ValueError, match = "API key is not set") :
+            app_setting.get_api_key()
