94bc6186d4f76af5527b525beaa437e7b82cc588
Mohammad Hossein Heydarchi <mohammad.heydarchi@volvocars.com>
2023-04-16 20:14:08 +0200

    Imlement test units for SearchManager

---
+++ app/test/modules/logic/MockSearchApi.py
@@ -0,0 +1,19 @@
+# This class implements a mock search api. It is used for testing purposes.
+from modules.search.AbstractSearchApi import AbstractSearchApi
+from modules.search.SearchResult import SearchResult
+from modules.search.SearchEngine import SearchEngine
+
+
+class MockSearchApi(AbstractSearchApi):
+    def __init__(self):
+        self.search_engine_id = SearchEngine.MOCK
+
+    def search(self, query, num_results=10):
+        result = []
+        for i in range(0, num_results):
+            search_result = SearchResult()
+            search_result.title = f"Title {i}"
+            search_result.url = f"https://www.example.com/{i}"
+            search_result.description = f"Description {i}"
+            result.append(search_result)
+        return result
---
+++ app/test/modules/logic/SearchManager_test.py
@@ -0,0 +1,26 @@
+# This class tests the SearchManager class by using the MockSearchApi class.
+import unittest
+from modules.logic.SearchManager import SearchManager
+from test.modules.logic.MockSearchApi import MockSearchApi
+
+
+class SearchManager_test(unittest.TestCase):
+    def test_search(self):
+        # Initialize search APIs
+        mock_search_api = MockSearchApi()
+
+        # Initialize SearchManager with a list of search APIs
+        multi_search = SearchManager()
+        multi_search.add_search_api(mock_search_api)
+
+        # Set query
+        query = "python programming"
+
+        # Define callback function
+        def search_callback(results):
+            for search_engine_id, result in results.items():
+                print(f"Search results from {search_engine_id}:")
+                print(result)
+
+        # Run search
+        multi_search.run_search(query, search_callback)
